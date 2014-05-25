8a19ff071e3e8054e3f82f0c522d22b18676cc4d
Nazareno <nazareno@computacao.ufcg.edu.br>
2014-05-25 18:56:16 -0300

    versão gerando tudo das distâncias por ano e idade

--- distances.R
+++ distances.R
@@ -7,27 +7,42 @@ require("GGally")
 setwd('/Users/nazareno/Documents/workspace/urbanform_analyses')
 source("../lifecourse_analyses/mobility_functions.R")
 
+corrigir_distancias <- function(distances){
+  distances[distances == -1] <- NA
+  distances$House <- as.factor(distances$House)
+  distances$ID <- as.factor(distances$ID)
+  distances <- distances[tolower(distances$city) %in% get_all_cities_lc(),]
+  distances$city <- NULL
+  
+  # CORREÇÕES
+  distances[distances$ID == 46 & distances$House == 4,]$bos_distance <- 2927.73
+  distances[distances$ID == 261 & distances$House == 1,]$bos_distance <- 2919.11
+  distances[distances$ID == 336 & distances$House == 1,]$bus_distance <- 260.9
+  distances[distances$ID == 4 & distances$House == 5,]$bus_distance <- NA
+  distances
+}
+
+adiciona_fator_niveldedistancia <- function(distances.long){
+  within(distances.long, 
+       part <- as.factor(cut(value, 
+                             DISTANCE_LEVELS, 
+                             include.lowest=TRUE)))
+}
+
+# Limites inferiores e superiores dos níveis em que separamos as distâncias
+# (para perto, médio, longe)
+DISTANCE_LEVELS <- c(0, 1, 3, 5, 10, 20, 100)
+
 theme_set(theme_bw())
 
 # Lendo as distâncias de cada casa 
 distances <- read.csv('dados/House-postcode-degreejunto.txt')
 distances <- distances[,c(1, 36, 25:33, 24)]
-distances[distances == -1] <- NA
-distances$House <- as.factor(distances$House)
-distances$ID <- as.factor(distances$ID)
-distances <- distances[tolower(distances$city) %in% get_all_cities_lc(),]
-distances$city <- NULL
-
-# CORREÇÕES
-distances[distances$ID == 46 & distances$House == 4,]$bos_distance <- 2927.73
-distances[distances$ID == 261 & distances$House == 1,]$bos_distance <- 2919.11
-distances[distances$ID == 336 & distances$House == 1,]$bus_distance <- 260.9
-distances[distances$ID == 4 & distances$House == 5,]$bus_distance <- NA
-
+distances <- corrigir_distancias(distances)
 distances.long <- melt(distances, id.vars = c("ID", "House"))
-
 # m -> km
 distances.long$value <- distances.long$value / 1000
+
 cdf <- ddply(distances.long, "variable", summarise, value.mean=mean(value, na.rm = TRUE))
 
 # HISTOGRAMAS:
@@ -49,13 +64,147 @@ ggpairs(na.omit(distances)/1000, columns = 3:11)
 #dev.copy(pdf, "scatterplot-matrix-distances.pdf", width = 16, height = 12)
 dev.off()
 
-# PAREI AQUI.
-# REDUZIR DIMENSÕES?
+# TIRANDO SUBRAY E TRAM
+distances.long <- subset(distances.long, variable != "subway_distance" & variable != "tram_distance")
+distances.long$variable <- droplevels(distances.long$variable)
+
+ggplot(distances.long, aes(x=value)) +
+  #geom_histogram(binwidth = 1) + 
+  facet_grid(. ~ variable, scales = "free" ) 
+
+distances.long <- adiciona_fator_niveldedistancia(distances.long)
+distances.long$part = factor(distances.long$part, rev(levels(distances.long$part)))
+
+# Salvando 3 figuras para aproveitar as melhores escalas
+pdf("distances-histograms-comfaixas1.pdf", width = 36, height = 3 )
+ggplot(distances.long, aes(x=value, fill = part)) +
+  geom_histogram( binwidth = .5) + 
+  facet_grid(. ~ variable, scales = "free" ) +
+  scale_fill_brewer(type = "seq", palette = "PuBu")
+dev.off()
+
+pdf("distances-histograms-comfaixas2.pdf", width = 36, height = 3 )
+ggplot(distances.long, aes(x=value, fill = part)) +
+  geom_histogram( binwidth = 1) + 
+  facet_grid(. ~ variable, scales = "free" ) +
+  scale_fill_brewer(type = "seq", palette = "PuBu")
+dev.off()
+
+pdf("distances-histograms-comfaixas3.pdf", width = 36, height = 3 )
+ggplot(distances.long, aes(x=value, fill = part)) +
+  geom_histogram() + 
+  facet_grid(. ~ variable, scales = "free" ) +
+  scale_fill_brewer(type = "seq", palette = "PuBu")
+dev.off()
 
 # Lendo demais dados dos respondents para pegar início e fim das casas e idades
 data_file = "dados/verhuisgeschiedenis_350.csv"
 resps.f.long <- ler_e_corrigir_casas(data_file)
+
+distances <- read.csv('dados/House-postcode-degreejunto.txt')
+distances <- distances[,c(1, 36, 25:33, 24)]
+distances <- corrigir_distancias(distances)
+# TIRANDO SUBRAY E TRAM
+distances$subway_distance<- NULL
+distances$tram_distance <- NULL
+
 distances$time <- distances$House
 distances$House <- NULL
 resps.f.long$verv1werk <- NULL
 resps.f.long <- merge(resps.f.long, distances, by = c("ID", "time"))
+
+
+# m -> km
+resps.f.long[,6:12] <- resps.f.long[,6:12] / 1000
+distances.all <- melt(resps.f.long, id.vars = 1:5, measure.vars= 6:12)
+distances.all <- adiciona_fator_niveldedistancia(distances.all)
+distances.all$value <- NULL
+distances.all <- rename(distances.all, c(variable = "distance", part = "value",  time = "house"))
+
+plot_distance_sequence <- function(distances.all, distance_focused, pal){
+  states_alph <- levels(distances.all$value)
+  states_pal <- rev(brewer.pal(6, pal))
+  
+  focused_data <- subset(distances.all, distance == distance_focused)
+  
+  resps.seq <- seqformat(focused_data, id = "ID", 
+                         from = "SPELL", to = "STS", 
+                         process = FALSE, 
+                         begin = "woonbeg", end = "woonend", status = "value")
+  
+  resps.seq <- seqdef(resps.seq, 
+                      #left = 0, 
+                      #right = 0, gaps = 0,
+                      alphabet = states_alph)
+  
+  # comprimento das sequencias excluindo repeticoes consecutivas de estados:
+  table(seqlength(seqdss(resps.seq)))
+  table(seqlength(seqdss(resps.seq))) / NROW(resps.seq)
+  mean(seqlength(seqdss(resps.seq)))
+  
+  #print(resps.seq[1:10,], format = "STS")  
+  just_the_plots(resps.seq, paste0(distance_focused, "-abs"), states_pal)
+  
+  focused_data.idade <- focused_data[!is.na(focused_data$geborenjaar) & focused_data$geborenjaar != 999,]
+  focused_data.idade$woonbeg <- focused_data.idade$woonbeg- focused_data.idade$geborenjaar
+  focused_data.idade$woonend <- focused_data.idade$woonend- focused_data.idade$geborenjaar
+  
+  resps.seq.i <- seqformat(focused_data.idade, id = "ID", 
+                           from = "SPELL", to = "STS", 
+                           process = FALSE, 
+                           begin = "woonbeg", end = "woonend", status = "value")
+  
+  resps.seq.i <- seqdef(resps.seq.i, 
+                        #left = 0, 
+                        #right = 0, gaps = 0,
+                        alphabet = states_alph)
+  just_the_plots(resps.seq.i, paste0(distance_focused, "-age"), states_pal)  
+}
+
+just_the_plots <- function(resps.seq, distance_focused, states_pal){
+  pdf(paste0("distance-abs-exemplo-", distance_focused,".pdf"), width = 6, height = 4 )
+  seqiplot(resps.seq, border = NA, withlegend = "right", cex.legend = 0.5, missing.color = "white", cpal = states_pal)
+  dev.off()
+  
+  pdf(paste0("distance-abs-todos-end-", distance_focused,".pdf"), width = 6, height = 4 )
+  seqIplot(resps.seq, withlegend = "right", missing.color = "white", sortv = "from.end", cex.legend = 0.5, cpal = states_pal)
+  dev.off()
+  
+  pdf(paste0("distance-abs-todos-start-", distance_focused,".pdf"), width = 6, height = 4 )
+  seqIplot(resps.seq, withlegend = "right", missing.color = "white", sortv = "from.start", cex.legend = 0.5, cpal = states_pal)
+  dev.off()
+  
+  pdf(paste0("distance-dist-", distance_focused,".pdf"), width = 6, height = 4 )
+  seqdplot(resps.seq, withlegend = "right", cex.legend = 0.5, cpal = states_pal)
+  dev.off()
+}
+
+plot_distance_sequence(distances.all, 
+                       distance_focused = "ic_distance", 
+                       pal = "PuRd")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "bus_distance", 
+                       pal = "BuGn")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "lt_distance", 
+                       pal = "GnBu")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "park_distance", 
+                       pal = "PuBuGn")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "bos_distance", 
+                       pal = "Reds")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "retail_distance", 
+                       pal = "YlGnBu")
+plot_distance_sequence(distances.all, 
+                       distance_focused = "highway_distance", 
+                       pal = "YlOrBr")
+
+
+#seqfplot(resps.seq,  border = NA, withlegend = "right", missing.color = "white", 
+#         weighted = FALSE, cex.legend = 0.5)
+
+pdf("distance-abs-freq.pdf", width = 6, height = 4 )
+seqdplot(resps.seq, withlegend = "right")
+dev.off()
